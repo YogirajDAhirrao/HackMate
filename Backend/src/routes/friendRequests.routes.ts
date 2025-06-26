77b5b896890784ec1e99207ff2180e8244b35485
YogirajDAhirrao <yogirajahirrao@gmail.com>
2025-06-26 15:13:02 +0530

    added model and fixes

--- Backend/src/routes/friendRequests.routes.ts
+++ Backend/src/routes/friendRequests.routes.ts
@@ -1,5 +1,31 @@
 import { Router } from "express";
+import {
+  sendFriendRequest,
+  acceptFriendRequest,
+  rejectFriendRequest,
+  getIncomingFriendRequests,
+  getOutgoingFriendRequests,
+  getFriends,
+} from "../controllers/friendRequestController.js";
 
 const friendRequestRouter = Router();
+
+// Send friend request
+friendRequestRouter.post("/:userId", sendFriendRequest);
+
+// Accept friend request
+friendRequestRouter.post("/:requestId/accept", acceptFriendRequest);
+
+// Reject friend request
+friendRequestRouter.post("/:requestId/reject", rejectFriendRequest);
+
+// Get incoming friend requests
+friendRequestRouter.get("/incoming", getIncomingFriendRequests);
+
+// Get outgoing friend requests
+friendRequestRouter.get("/outgoing", getOutgoingFriendRequests);
+
+// Get all friends
+friendRequestRouter.get("/friends", getFriends);
 
 export default friendRequestRouter;
