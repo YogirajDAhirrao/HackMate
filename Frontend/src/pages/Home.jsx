cb771d1201a8484ff9ac5d1b5090c945c24b2cc9
YogirajDAhirrao <yogirajahirrao@gmail.com>
2025-09-27 20:05:28 +0530

    fixes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import AuthenticatedHome from "../ui/AuthenticatedHome";
 import UnauthenticatedHome from "../ui/UnauthenticatedHome";
@@ -5,12 +6,28 @@ import "./Home.css";
 
 function Home() {
   const { user, loading } = useAuth();
+  const [showBackendMessage, setShowBackendMessage] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (loading) {
+      // after 3 seconds, show "Spinning up backend…" instead of just loading
+      timer = setTimeout(() => {
+        setShowBackendMessage(true);
+      }, 3000);
+    } else {
+      setShowBackendMessage(false); // reset when not loading
+    }
+    return () => clearTimeout(timer);
+  }, [loading]);
 
   if (loading)
     return (
       <div className="spinner-container">
         <div className="spinner"></div>
-        <p className="loading-text">Spinning up the backend…</p>
+        <p className="loading-text">
+          {showBackendMessage ? "Spinning up the backend…" : "Loading…"}
+        </p>
       </div>
     );
 
